cf42474ffb35c3863ea701f4ab4c7dfa12bdfbd9
maxarvid <max.arvid.anderson@gmail.com>
2022-01-10 18:25:16 +0100

    adds radio grid component and passes 2nd spec

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import React from "react";
+import CellGrid from "./components/CellGrid";
+
 
 const App = () => {
   return (
+    <div>
     <h1 data-cy="title">
      Conway's Game of Life
     </h1>
+    <CellGrid />
+    </div>
   );
 };
 
